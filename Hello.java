d794037483a1e465dc9d5f42b4239184707ffc6a
abhishekpawar1060 <abhishekpawar7218@gmail.com>
2024-05-29 09:51:55 +0530

    first commit

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 public class Hello{
 	public static void main(String[] args){
-[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D	system.out.print("Hello World");
+		System.out.print("Hello World");
 	}
 }
